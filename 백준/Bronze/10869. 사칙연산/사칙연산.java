3c0a587b491efb9c357d2afde9e348c1767a4932
minji <104603852+mjkim05@users.noreply.github.com>
2024-05-17 23:07:09 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d\n%d\n%d\n%d\n%d\n", a+b, a-b, a*b, a/b, a%b);
+    }
+}
