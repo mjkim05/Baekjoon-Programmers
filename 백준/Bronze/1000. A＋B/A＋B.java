3e0b133cdb06d2918f311b5be5f3410d9745065e
minji <104603852+mjkim05@users.noreply.github.com>
2024-05-17 22:56:56 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 12960 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.printf("%d", a+b);
+    }
+    
+}    
